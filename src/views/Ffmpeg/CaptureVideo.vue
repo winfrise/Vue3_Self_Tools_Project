b6c4917f7a31b730262e461fa9131bdc3488709c
Git_Winfrise <>
2025-12-27 22:02:35 +0800

    代码优化

--- src/views/Ffmpeg/CaptureVideo.vue
+++ src/views/Ffmpeg/CaptureVideo.vue
@@ -2,7 +2,8 @@
   <el-container style="width: 1200px">
     <el-main>
       <el-card>
-          <cropper-canvas class="cropper-canvas" v-if="isMounted"
+          <cropper-canvas style="width: 800px; height: 600px;" v-if="isMounted" 
+            ref="cropperCanvasRef"
             :background="true"
             :disabled="false"
             :scaleStep="0.1"
@@ -10,7 +11,7 @@
           >
 
             <cropper-image
-              ref="cropperImage"
+              ref="cropperImageRef"
               initial-center-size="contain"
               :rotatable="true"
               :scalable="true"
@@ -21,55 +22,37 @@
             />
 
             <!-- 遮罩效果 -->
-            <cropper-shade
-              theme-color="rgba(0, 0, 0, 0.5)"
-            />
-            <cropper-handle
-              action="select",
-              theme-color="rgba(51, 153, 255, 0.5)"
-              :plain="true"
-            />
+            <cropper-shade theme-color="rgba(0, 0, 0, 0.5)" />
+            <cropper-handle action="select" theme-color="rgba(51, 153, 255, 0.5)" :plain="true" />
 
             <cropper-selection
-                :x="selection.x"
-                :y="selection.y"
-                :width="selection.width"
-                :height="selection.height"
-                :aspect-ratio="selection.aspectRatio"
-                :initial-coverage="selection.initialCoverage"
-                :hidden="selection.hidden"
-                :initial-aspect-ratio="selection.initialAspectRatio"
-                :movable="selection.movable"
-                :resizable="selection.resizable"
-                :zoomable="selection.zoomable"
-                :multiple="selection.multiple"
-                :keyboard="selection.keyboard"
-                :outlined="selection.outlined"
-                :precise="selection.precise"
-                :dynamic="selection.dynamic"
-              @change="onSelectionChange"
+              :x="selection.x"
+              :y="selection.y"
+              :width="selection.width"
+              :height="selection.height"
+              :aspect-ratio="selection.aspectRatio"
+              :initial-coverage="selection.initialCoverage"
+              :initial-aspect-ratio="selection.initialAspectRatio"
+              :movable="true"
+              :resizable="true"
+              @change="onCropperSelectionChange"
             >
-                  <!-- 网格 -->
-                  <cropper-grid
-                      :rows="3"
-                      :columns="3"
-                      :bordered="true"
-                      :covered="true",
-                      themeColor="rgba(0, 0, 0, 0.5)"
-                  />
+              <!-- 网格 -->
+              <cropper-grid :rows="3" :columns="3" :bordered="true" :covered="true" themeColor="rgba(0, 0, 0, 0.5)" />
 
-                  <!-- 十字准线 -->
-                  <cropper-crosshair
-                      :centered="true"
-                      themeColor="rgba(0, 0, 0, 0.5)"
-                  />
-                  <cropper-handle
-                    v-for="subhandle in handles"
-                    :key="subhandle.action"
-                    :action="subhandle.action"
-                    :hidden="subhandle.hidden"
-                    :theme-color="subhandle.themeColor"
-                  />
+              <!-- 十字准线 -->
+              <cropper-crosshair :centered="true" themeColor="rgba(0, 0, 0, 0.5)" />
+
+              <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)" />
+              <cropper-handle action="n-resize" theme-color="#3399ff" />
+              <cropper-handle action="e-resize" theme-color="#3399ff" />
+              <cropper-handle action="s-resize" theme-color="#3399ff" />
+              <cropper-handle action="w-resize" theme-color="#3399ff" />
+              <cropper-handle action="ne-resize" theme-color="#3399ff" />
+              <cropper-handle action="nw-resize" theme-color="#3399ff" />
+              <cropper-handle action="se-resize'" theme-color="#3399ff" />
+              <cropper-handle action="sw-resize" theme-color="#3399ff" />
+              
             </cropper-selection>
           </cropper-canvas>
       </el-card>
@@ -84,7 +67,7 @@
             <el-descriptions-item label="width">{{ selectionData.width }}</el-descriptions-item>
             <el-descriptions-item label="height">{{ selectionData.height }}</el-descriptions-item>
         </el-descriptions>
-        
+
         <el-descriptions size="large" :column="1" title="Image Data">
             <el-descriptions-item label="scaleX">{{ imageData.scaleX }}</el-descriptions-item>
             <el-descriptions-item label="scaleY">{{ imageData.scaleY }}</el-descriptions-item>
@@ -103,6 +86,7 @@
 <script setup lang="ts">
   import { CropperCanvas, CropperImage, CropperSelection, CropperCrosshair, CropperGrid, CropperHandle, CropperShade  } from 'cropperjs';
   import { onMounted, reactive, ref, nextTick } from 'vue';
+  import type { Selection } from '@cropper/element-selection';
 
   const isMounted = ref(false)
 
@@ -120,93 +104,65 @@
     aspectRatio: undefined,
     initialAspectRatio: undefined,
     initialCoverage: 0.5,
-    dynamic: false,
-    movable: true,
-    resizable: true,
-    zoomable: false,
-    multiple: false,
-    keyboard: false,
-    outlined: false,
-    precise: false,
   })
 
-  const handles = ref([
-        {
-          hidden: false,
-          action: 'move',
-          themeColor: 'rgba(255, 255, 255, 0.35)',
-        },
-        {
-          hidden: false,
-          action: 'n-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'e-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 's-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'w-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'ne-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'nw-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'se-resize',
-          themeColor: '#3399ff',
-        },
-        {
-          hidden: false,
-          action: 'sw-resize',
-          themeColor: '#3399ff',
-        },
-      ])
+      const imageData = ref({
+        scaleX: 0,
+        skewY: 0,
+        skewX: 0,
+        scaleY: 0,
+        translateX: 0,
+        translateY: 0,
+      })
 
-const imageData = ref({
-  scaleX: 0,
-  skewY: 0,
-  skewX: 0,
-  scaleY: 0,
-  translateX: 0,
-  translateY: 0,
-})
-
-const selectionData = ref({
-  x: 0,
-  y: 0,
-  width: 0,
-  height: 0,
-})
+      const selectionData = ref({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+      })
 
 const onImageTransform = (event: CustomEvent) => {
   const [ scaleX, skewY, skewX, scaleY, translateX, translateY ] = event.detail.matrix
   imageData.value = {scaleX, scaleY, skewX, skewY, translateX, translateY}
 }
 
-const onSelectionChange = (event: CustomEvent) => {
-  selectionData.value = event.detail;
-}
+
+const cropperCanvasRef = ref()
+const cropperImageRef = ref()
+const onCropperSelectionChange = (event: CustomEvent) => {
+  const cropperCanvas = cropperCanvasRef.value as CropperCanvas;
+
+  if (!cropperCanvas) {
+    return;
+  }
+
+  const cropperCanvasRect = cropperCanvas.getBoundingClientRect();
+  const selection = event.detail as Selection;
+
+  const cropperImage = cropperImageRef.value as CropperImage;
+      const cropperImageRect = cropperImage.getBoundingClientRect();
+      const maxSelection: Selection = {
+        x: cropperImageRect.left - cropperCanvasRect.left,
+        y: cropperImageRect.top - cropperCanvasRect.top,
+        width: cropperImageRect.width,
+        height: cropperImageRect.height,
+      };
+
+      if (!checkInSelection(selection, maxSelection)) {
+        event.preventDefault();
+        selectionData.value = event.detail;
+      }
+  }
+
+  // 验证是否在图片范围之内
+  const checkInSelection = (selection: Selection, maxSelection: Selection) => {
+      return (
+        selection.x >= maxSelection.x
+        && selection.y >= maxSelection.y
+        && (selection.x + selection.width) <= (maxSelection.x + maxSelection.width)
+        && (selection.y + selection.height) <= (maxSelection.y + maxSelection.height)
+      );
+  }
 </script>
 
-<style lang="scss" scoped>
-  .cropper-canvas {
-    width: 800px;
-    height: 600px;
-  }
-</style>
